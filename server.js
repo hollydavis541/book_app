6d79f977f14429ac822c7e17e3d39a9e4e526013
Holly Davis <hollydavis541@gmail.com>
2019-11-15 09:39:07 -0800

    added deleteBook function

--- server.js
+++ server.js
@@ -110,7 +110,11 @@ function updateBook(request, response) {
 
 // Allows user to delete a book from the database
 function deleteBook(request, response) {
-  //
+  let SQL = 'DELETE FROM books WHERE id=$1;';
+  let values = [request.params.id];
+  return client.query(SQL, values)
+    .then(response.redirect('/'))
+    .catch(handleError);
 }
 
 function handleError(error, response) {
